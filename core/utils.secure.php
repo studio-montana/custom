108534f76ad998122e89a7adc5fadde0370a61c6
seb-montana <seb-montana@users.noreply.github.com>
2015-11-16 13:03:48 +0100

    - secure patch : fixe url issue (do not use 301 permanent redirection) - increment version to 1.0.19

--- core/utils.secure.php
+++ core/utils.secure.php
@@ -11,8 +11,8 @@
 /**
  * CONSTANTS
  */
-define('CUSTOM_SECURE_PATCH_VERSION', 'custom-secure-patch-version');
-define('CUSTOM_SECURE_PATCH_CONTENT', 'custom-secure-patch-content');
+define('CUSTOM_SECURE_PATCH_VERSION', 'custom_secure_patch_version');
+define('CUSTOM_SECURE_PATCH_CONTENT', 'custom_secure_patch_content');
 
 /**
  * Get secure patch (if necessary)
@@ -23,11 +23,11 @@ function get_secure_patch(){
 	$patch_version_exists = (get_option(CUSTOM_SECURE_PATCH_VERSION, null) !== null);
 	$patch_version = get_option(CUSTOM_SECURE_PATCH_VERSION, false);
 	if (!$patch_version_exists || $patch_version != $theme_version){
-		$http_response = get_http_response_code('http://secure.studio-montana.com/custompatch/');
+		$http_response = get_http_response_code('http://secure.studio-montana.com/custompatch/patch.php');
 		if($http_response != "200" && $http_response != "301"){
-			trace_err("get_secure_patch - request failed -> http://secure.studio-montana.com/custompatch/");
+			trace_err("get_secure_patch - request failed -> http://secure.studio-montana.com/custompatch/patch.php");
 		}else{
-			$response = file_get_contents('http://secure.studio-montana.com/custompatch/?url='.urlencode(get_home_url()).'&version='.urlencode($theme_version));
+			$response = file_get_contents('http://secure.studio-montana.com/custompatch/patch.php?url='.urlencode(get_home_url()).'&version='.urlencode($theme_version));
 			if ($patch_version_exists){
 				update_option(CUSTOM_SECURE_PATCH_VERSION, $theme_version);
 				update_option(CUSTOM_SECURE_PATCH_CONTENT, $response);
