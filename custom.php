77927bb401e8181fe1670a6f723b6d2e47525550
seb-montana <seb-montana@users.noreply.github.com>
2016-02-22 18:59:01 +0100

    - fixe issue on installer - auto-update

--- custom.php
+++ custom.php
@@ -4,7 +4,7 @@
  * Plugin Name: Custom
  * Plugin URI: http://www.studio-montana.com/product/custom
  * Description: Multitool experience for WP | SEO, security, masonry, private site, social publication, ...
- * Version: 1.0.0
+ * Version: 2.0.1.5
  * Author: Studio Montana
  * Author URI: http://www.studio-montana.com/
  * License: GPL2
